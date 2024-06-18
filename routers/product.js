3131bcc38182bdd9d85b66a9a91ee469ca25e324
fapvelder <lyhungphat25@gmail.com>
2024-06-18 16:09:14 +0700

    fix function

--- routers/product.js
+++ routers/product.js
@@ -2,12 +2,14 @@ import express from "express";
 import {
   createProduct,
   deleteProduct,
+  getProductById,
   getProducts,
   updateProduct,
 } from "../controllers/product.js";
 
 const router = express.Router();
 router.get("/", getProducts);
+router.get("/:id", getProductById);
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
